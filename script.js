aa7f85966d4e21dd17835d12bc8e05b41e15a314
brianjckim <bkim2102@gmail.com>
2018-06-26 16:45:31 -0400

    second commit

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 const players = document.getElementById("players");
 const addPlayerButton = document.getElementById("add-player-button");
-const takePhotoButton = document.getElementById("take-photo-button");
-const submitPlayersButton = document.getElementById("submit-players-button");
+const startGameButton = document.getElementById("start-game-button");
+const getDataButton = document.getElementById("get-data-button");
 
 const addPlayer = () => {
   const player = document.createElement("input");
   const breakSpace = document.createElement("br");
+  const getData = document.createElement("button");
   player.setAttribute("type", "text");
   player.setAttribute("name", "Player");
   player.setAttribute("id", "player");
   player.setAttribute("maxlength", "10");
   player.setAttribute("placeholder", "Enter name...");
+  getData.setAttribute("id", "get-data-button");
+  getData.append("Get Data");
   players.appendChild(breakSpace);
   players.appendChild(player);
+  players.appendChild(getData);
 };
 
-const addPhoto = () => {};
-
-const submitPlayers = () => {};
+const getData = () => {
+  const degrees = document.createElement("h2");
+  const response = fetch("tessela.local:8080");
+  const json = response.json();
+  console.log(json);
+};
 
 addPlayerButton.addEventListener("click", addPlayer);
-takePhotoButton.addEventListener("click", addPhoto);
-submitPlayersButton.addEventListener("click", submitPlayers);
+getDataButton.addEventListener("click", getData);
